bef27434759578ba520934c786239cd53c67da7c
Pratik Kharche <80400646+patt26@users.noreply.github.com>
2023-04-04 20:32:39 +0530

    Deleting...

--- employeeDirectory_UI/src/main/java/com/learning/employeerestdemo/Interface/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.learning.employeerestdemo.Interface;
-
-import com.learning.employeerestdemo.Model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-
-}
